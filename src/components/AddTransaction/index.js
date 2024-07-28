447032412ae5f0f9b6559acd78a69a621eb6b246
Govardhan <govardhanmani447@gmail.com>
2024-07-28 03:01:02 +0000

    pedding fecting

--- src/components/AddTransaction/index.js
+++ src/components/AddTransaction/index.js
@@ -1,33 +1,107 @@
 import {Component} from 'react'
+import {IoIosSave} from 'react-icons/io'
+import {IoClose} from 'react-icons/io5'
 
 import './index.css'
 
 class AddTransaction extends Component {
   state = {
-    credit: 0,
-    debit: 0,
-    balence: 0,
     description: '',
-    transactionDetail: '',
-    inputAmount: '',
+    transactionDetails: [],
+    inputAmount: 0,
+    transactionType: 'credit',
+  }
+
+  onchangeInputAmount = event => {
+    this.setState({inputAmount: event.target.value})
+  }
+
+  onchangeInputDescription = event => {
+    this.setState({description: event.target.value})
+  }
+
+  onchangeTransactions = event => {
+    this.setState({transactionType: event.target.value})
+  }
+
+  onsubmitForm = async () => {
+    const {inputAmount, transactionType, description} = this.state
+
+    const newList = {
+      date: new Date(),
+      description,
+      transactionType,
+      inputAmount,
+    }
+
+    const url = 'https://apis.ccbp.in/login'
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(newList),
+    }
+
+    const response = await fetch(url, options)
+    const data = await response.json()
+    if (response.ok === true) {
+      this.onSubmitSuccess(data.jwt_token)
+    } else {
+      this.onSubmitFailure(data.error_msg)
+    }
+
+    this.setState(prevState => ({
+      transactionDetails: [...prevState.transactionDetails, newList],
+    }))
   }
 
   render() {
-    const {inputAmount} = this.state
+    const {inputAmount, transactionDetails} = this.state
     return (
-      <div className="add-container">
+      <form className="add-container" onSubmit={this.onsubmitForm}>
         <div className="transaction-type-container">
-          <lable>Transaction Type</lable>
-          <select>
+          <lable className="lable">Transaction Type</lable>
+          <select
+            className="select-container"
+            onChange={this.onchangeTransactions}
+          >
             <option value="credit">Credit</option>
             <option value="debit">Debit</option>
           </select>
         </div>
-        <div>
-          <lable>Transaction Type</lable>
-          <input type="number" value={inputAmount} />
+        <div className="transaction-type-container">
+          <lable className="lable">Amount</lable>
+          <input
+            className="select-container"
+            type="number"
+            value={inputAmount}
+            onChange={this.onchangeInputAmount}
+          />
         </div>
-      </div>
+        <div className="transaction-type-container">
+          <lable className="lable">Description</lable>
+          <textarea
+            className="text-container"
+            rows="4"
+            cols="68"
+            onChange={this.onchangeInputDescription}
+          >
+            {}
+          </textarea>
+        </div>
+        <div className="button-container">
+          <button type="submit" className="save-btn">
+            <IoIosSave className="icon" />
+            SAVE
+          </button>
+          <button
+            type="button"
+            className="close-btn"
+            onClick={this.onclickClose}
+          >
+            <IoClose className="icon" />
+            CANCEL
+          </button>
+        </div>
+      </form>
     )
   }
 }
